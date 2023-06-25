703545030f66ff6b147f412e843079a4b415d51b
EddieUFSM <eddie.maneki@gmail.com>
2023-06-25 14:31:44 -0300

    add successfully tests to structures utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,5 @@
-const fs = require('fs')
-const chalk = require('chalk')
+import fs from 'fs';
+import chalk from 'chalk';
 
 const insertContent = (fileName, content) => {
   fs.appendFile(fileName, content, (err) => {
